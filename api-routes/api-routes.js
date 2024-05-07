eaa88170af1d01216b7f171f1ec08f7381b9e90b
Dawson Knudtson <300078204@fvtc.edu>
2024-05-07 15:24:04 -0500

    I think I got it to work - rest test works

--- api-routes/api-routes.js
+++ api-routes/api-routes.js
@@ -17,17 +17,14 @@ router.post('/', async (request, response) => {
 router.put('/:id', async (request, response) => {
     const { id } = request.params
 
-    const collection = await getCollection('Todo-API', 'Todos');
+    const collection  = await getCollection('Todo-API', 'Todos') 
+	const todo = await collection.findOne({ _id: new ObjectId(id) })
 
-    // not sure why this is not working
-    const todo = await collection.findOne({ _id: new ObjectId(id) })
-    
-    //Toggle
-    
     const complete = !todo.complete
-    const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { complete } })
-    response.json({ id,complete });
 
+    const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { complete } })
+    response.json(todo)
+    
 });
 
 
